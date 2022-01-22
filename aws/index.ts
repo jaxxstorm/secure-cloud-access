da235441c594fb647d7f3544b86be39ff4d51da1
Lee Briggs <lee@leebriggs.co.uk>
2022-01-22 13:34:43 -0800

    add ReadOnly access

--- aws/index.ts
+++ aws/index.ts
@@ -2,28 +2,40 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const oidcProvider = new aws.iam.OpenIdConnectProvider("secure-cloud-access", {
-    thumbprintLists: [ "6938fd4d98bab03faadb97b34396831e3780aea1" ],
-    clientIdLists: [ "https://github.com/jaxxstorm", "sts.amazonaws.com" ],
-    url: "https://token.actions.githubusercontent.com"
-})
+  thumbprintLists: ["6938fd4d98bab03faadb97b34396831e3780aea1"],
+  clientIdLists: ["https://github.com/jaxxstorm", "sts.amazonaws.com"],
+  url: "https://token.actions.githubusercontent.com",
+});
 
 const role = new aws.iam.Role("secure-cloud-access", {
-    description: "Access for github.com/jaxxstorm/secure-cloud-access",
-    assumeRolePolicy: {
-        Version: "2012-10-17",
-        Statement: [{
-            Action: ["sts:AssumeRoleWithWebIdentity"],
-            Effect: "Allow",
-            Condition: {
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": "repo:jaxxstorm/aws-secure-access:*"
-                },
-            },
-            Principal: {
-                Federated: [oidcProvider.arn]
-            }
-        }]
-    } as aws.iam.PolicyDocument
-})
+  description: "Access for github.com/jaxxstorm/secure-cloud-access",
+  assumeRolePolicy: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: ["sts:AssumeRoleWithWebIdentity"],
+        Effect: "Allow",
+        Condition: {
+          StringLike: {
+            "token.actions.githubusercontent.com:sub":
+              "repo:jaxxstorm/aws-secure-access:*",
+          },
+        },
+        Principal: {
+          Federated: [oidcProvider.arn],
+        },
+      },
+    ],
+  } as aws.iam.PolicyDocument,
+});
 
-export const roleArn = role.arn
+const partition = aws.getPartition();
+
+partition.then((p) => {
+  new aws.iam.PolicyAttachment("readOnly", {
+    policyArn: `arn:${p.partition}:iam::aws:policy/ReadOnlyAccess`,
+    roles: [role.name],
+  });
+});
+
+export const roleArn = role.arn;
