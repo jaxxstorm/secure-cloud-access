39b6e2fe924794826933fa8d4f8ca0b2fabfa5cc
Lee Briggs <lee@leebriggs.co.uk>
2022-01-23 11:45:42 -0800

    try new identity provider

--- gcp/index.ts
+++ gcp/index.ts
@@ -2,7 +2,9 @@ import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 import * as google from "@pulumi/google-native";
 
-const serviceAccount = new google.iam.v1.ServiceAccount("github-actions", {
+const name = "github-actions"
+
+const serviceAccount = new google.iam.v1.ServiceAccount(name, {
     accountId: "github-actions"
 })
 
@@ -13,14 +15,14 @@ new gcp.projects.IAMMember("github-actions", {
 
 const identityPool = new gcp.iam.WorkloadIdentityPool("github-actions", {
   disabled: false,
-  workloadIdentityPoolId: "github-actions-1",
+  workloadIdentityPoolId: `${name}-3`,
 });
 
 const identityPoolProvider = new gcp.iam.WorkloadIdentityPoolProvider(
   "github-actions",
   {
     workloadIdentityPoolId: identityPool.workloadIdentityPoolId,
-    workloadIdentityPoolProviderId: "github-actions-1",
+    workloadIdentityPoolProviderId: `${name}-3`,
     oidc: {
       issuerUri: "https://token.actions.githubusercontent.com",
     },
@@ -36,7 +38,7 @@ const identityPoolProvider = new gcp.iam.WorkloadIdentityPoolProvider(
 new gcp.serviceaccount.IAMMember("repository", {
     serviceAccountId: serviceAccount.name,
     role: "roles/iam.workloadIdentityUser",
-    member: pulumi.interpolate`principalSet://iam.googleapis.com/${identityPool.name}/attribute.repository/jaxxstorm/repo`
+    member: pulumi.interpolate`principalSet://iam.googleapis.com/${identityPool.id}/attribute.repository/jaxxstorm/repo`
 })
 
 export const workloadIdentityProviderUrl = identityPoolProvider.name
